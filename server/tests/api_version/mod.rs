18a82cac0efd2fd7f4d03aa661fdca85ebdfabee
Josh Jones <ohblonddev@gmail.com>
2020-03-10 00:05:26 +0000

    require incorrect api version to return a 406

--- server/tests/api_version/mod.rs
+++ server/tests/api_version/mod.rs
@@ -33,10 +33,23 @@ async fn accept_header_uses_invalid_api_version() {
     let resp = request()
         .method(Method::GET.as_str())
         .path(get_request_endpoint_string("/greeting/hello").as_ref())
-        .header(
-            "accept",
-            format!("{}invalid+text", get_request_default_mime_prefix()),
-        )
+        .header("accept", "application/vnd.warpj.vinvalid+text")
+        .reply(&api)
+        .await;
+
+    asserting("returns NOT_ACCEPTABLE status code")
+        .that(&resp.status())
+        .is_equal_to(StatusCode::NOT_ACCEPTABLE);
+}
+
+#[tokio::test]
+async fn accept_header_uses_incorrect_api_version() {
+    let api = app_filters();
+
+    let resp = request()
+        .method(Method::GET.as_str())
+        .path(get_request_endpoint_string("/greeting/hello").as_ref())
+        .header("accept", "application/vnd.warpj.v2500+text")
         .reply(&api)
         .await;
 
