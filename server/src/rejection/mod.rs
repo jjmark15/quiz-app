3f7687c1e4a292173089432b0f3fc98073b73a87
Josh Jones <ohblonddev@gmail.com>
2020-03-14 03:26:37 +0000

    use shared logging trait to log rejections

--- server/src/rejection/mod.rs
+++ server/src/rejection/mod.rs
@@ -1,35 +1,35 @@
 use std::convert::Infallible;
 
-use log::warn;
+use log::{debug, warn};
 use serde::Serialize;
 use warp::http::StatusCode;
 use warp::reply::Json;
 use warp::Rejection;
 
 use crate::filters::validate::api_version::ApiValidationError;
+use crate::logging::{log_string, LogEntry, LogEntryKVP};
 
 pub async fn handle_api_validation_error(
     rej: Rejection,
 ) -> Result<warp::reply::WithStatus<Json>, Infallible> {
-    let code: warp::http::StatusCode;
-    let message: String;
+    let error_message: ErrorMessage;
+    let code: StatusCode;
 
     if rej.is_not_found() {
         code = StatusCode::NOT_FOUND;
-        message = "NOT_FOUND".to_string();
+        error_message = ErrorMessage::new(code.as_u16(), "NOT_FOUND".to_string());
     } else if let Some(err) = rej.find::<ApiValidationError>() {
         code = StatusCode::NOT_ACCEPTABLE;
-        message = err.description();
+        error_message = ErrorMessage::new(code.as_u16(), err.description());
     } else {
-        warn!("unhandled rejection: {:?}", rej);
         code = StatusCode::INTERNAL_SERVER_ERROR;
-        message = "UNHANDLED_REJECTION".to_string();
+        error_message = ErrorMessage::new(code.as_u16(), "UNHANDLED_REJECTION".to_string());
+        warn!("{}", log_string(&error_message));
     }
 
-    let json = warp::reply::json(&ErrorMessage {
-        code: code.as_u16(),
-        message,
-    });
+    debug!("{}", log_string(&error_message));
+
+    let json = warp::reply::json(&error_message);
     Ok(warp::reply::with_status(json, code))
 }
 
@@ -37,4 +37,20 @@ pub async fn handle_api_validation_error(
 struct ErrorMessage {
     code: u16,
     message: String,
+}
+
+impl ErrorMessage {
+    fn new(code: u16, message: String) -> ErrorMessage {
+        ErrorMessage { code, message }
+    }
+}
+
+impl LogEntry for ErrorMessage {
+    fn log_entry_kvps(&self) -> Vec<LogEntryKVP> {
+        vec![
+            LogEntryKVP::new("type", "rejection"),
+            LogEntryKVP::new("code", format!("{}", self.code)),
+            LogEntryKVP::new("message", &self.message),
+        ]
+    }
 }
