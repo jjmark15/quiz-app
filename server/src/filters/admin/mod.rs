215de32d6912c4640b9984c067b4c627b3d4a343
Josh Jones <ohblonddev@gmail.com>
2020-03-21 16:32:02 +0000

    refactor status filter to be in separate method
    closes #20

--- server/src/filters/admin/mod.rs
+++ server/src/filters/admin/mod.rs
@@ -3,9 +3,12 @@ use warp::Filter;
 use crate::handlers::admin;
 
 pub fn admin_filters() -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
-    let status = warp::path("status")
+    warp::path("admin").and(status())
+}
+
+fn status() -> impl Filter<Extract = (impl warp::Reply,), Error = warp::Rejection> + Clone {
+    warp::path("status")
         .and(warp::path::end())
         .and(warp::get())
-        .map(admin::status);
-    warp::path("admin").and(status)
+        .map(admin::status)
 }
