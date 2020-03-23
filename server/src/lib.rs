d33ab28492fb188709a5b0bec1f2d98ddfa21b85
Josh Jones <ohblonddev@gmail.com>
2020-03-23 00:14:04 +0000

    move app start method to lib to simplify main

--- server/src/lib.rs
+++ server/src/lib.rs
@@ -1,3 +1,5 @@
+use crate::filters::app_filters;
+
 pub(crate) mod config;
 pub(crate) mod error;
 pub mod filters;
@@ -5,3 +7,12 @@ pub(crate) mod handlers;
 pub(crate) mod logging;
 pub(crate) mod rejection;
 pub(crate) mod response;
+
+pub struct App;
+
+impl App {
+    pub async fn start(&self) {
+        const PORT: u16 = 3030;
+        warp::serve(app_filters()).run(([0, 0, 0, 0], PORT)).await;
+    }
+}
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,16 +1,10 @@
-#[macro_use]
-extern crate log;
 extern crate pretty_env_logger;
 
-use server::filters::app_filters;
+use server::App;
 
 #[tokio::main]
 async fn main() {
     pretty_env_logger::init();
-
-    let api = app_filters();
-
-    const PORT: u16 = 3030;
-    info!("welcome to my lovely server");
-    warp::serve(api).run(([0, 0, 0, 0], PORT)).await;
+    let app = App;
+    app.start().await;
 }
